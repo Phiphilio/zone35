f75c18807097ce0661f46e2985dc985736563472
erudit <philaoue@gmail.com>
2025-06-01 22:08:16 +0200

    utilisation de popup.ts et dplécement des fonctions vers le repertoire core

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,1 +1,25 @@
-// la logique vis à vis des popup
+import { calculerDureeSession, arreterSession, pad } from "../core/heure";
+
+let intervalId: null = null;
+let time = { hours: 0, minutes: 0, seconds: 0 };
+
+function afficherTemps() {
+  const affichage = document.getElementById("affichage");
+  if (affichage) {
+    affichage.innerText = `${pad(time.hours)}:${pad(time.minutes)}:${pad(
+      time.seconds
+    )}`;
+  }
+}
+
+document.getElementById("demarrer").addEventListener("click", () => {
+  if (!intervalId) {
+    time = { hours: 0, minutes: 0, seconds: 0 }; // reset
+    calculerDureeSession(afficherTemps);
+  }
+});
+
+document.getElementById("arreter").addEventListener("click", () => {
+  arreterSession();
+  intervalId = null;
+});
