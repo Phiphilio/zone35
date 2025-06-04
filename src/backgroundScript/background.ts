5841952b3ac89a838b65a67b598f474db4bcac13
erudit <philaoue@gmail.com>
2025-06-04 02:02:14 +0200

    gestion de la sauvegarde grâce à derniereDuereeSession

--- src/backgroundScript/background.ts
+++ src/backgroundScript/background.ts
@@ -1,8 +1,12 @@
-import { calculerDureeSession } from "../core/heure.js";
-import { TimerSession } from "../type/index.js";
+import {
+  calculerDureeSession,
+  sauvegarderDureeSession,
+} from "../core/heure.js";
+import { TimerSession, Time } from "../type/index.js";
 
 let timerSession: TimerSession | null = null;
-let time = { hours: 0, minutes: 0, seconds: 0 };
+let time: Time = { hours: 0, minutes: 0, seconds: 0 };
+let derniereDureeSession: Time = { hours: 0, minutes: 0, seconds: 0 };
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.command === "start") {
@@ -10,10 +14,12 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if (!timerSession) {
       time = { hours: 0, minutes: 0, seconds: 0 };
       timerSession = calculerDureeSession((time) => {
+        derniereDureeSession = time;
         chrome.runtime.sendMessage({ command: "update", time });
       });
     }
   } else if (message.command === "stop") {
+    sauvegarderDureeSession(derniereDureeSession);
     console.log("background dans start ");
     if (timerSession) {
       timerSession.end();
